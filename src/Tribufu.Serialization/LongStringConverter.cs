0d6f6357da31433929415d7590e004ef1a653f08
Guilherme Werner <guilhermewerner@tribufu.com>
2025-05-21 20:05:05 -0300

    Update LongStringConverter.cs

--- src/Tribufu.Serialization/LongStringConverter.cs
+++ src/Tribufu.Serialization/LongStringConverter.cs
@@ -10,7 +10,17 @@ namespace Tribufu.Serialization
     {
         public override ulong ReadJson(JsonReader reader, Type objectType, ulong existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            return ulong.Parse(reader.ToString());
+            if (reader.TokenType == JsonToken.String && ulong.TryParse(reader.Value?.ToString(), out var result))
+            {
+                return result;
+            }
+
+            if (reader.TokenType == JsonToken.Integer)
+            {
+                return Convert.ToUInt64(reader.Value);
+            }
+
+            throw new JsonSerializationException($"Unexpected token {reader.TokenType} when parsing ulong.");
         }
 
         public override void WriteJson(JsonWriter writer, ulong value, JsonSerializer serializer)
